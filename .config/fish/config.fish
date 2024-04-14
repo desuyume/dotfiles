0adc24a6408a54097f7f96dee9dde89cc7efa0c2
desuyume <77desss@gmail.com>
2024-04-14 14:57:32 +0200

    Arch Linux - Hyprland - dotfiles update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,18 +2,4 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-# dnf
-alias up="sudo dnf upgrade --refresh --best --allowerasing -y && flatpak update -y"
-
-# lampp
-alias lampp_start="sudo /opt/lampp/lampp start"
-alias lampp_stop="sudo /opt/lampp/lampp stop"
-
-# pnpm
-set -gx PNPM_HOME "/home/des/.local/share/pnpm"
-set -gx PATH "$PNPM_HOME" $PATH
-# pnpm end
-
-# go
-set -gx PATH "$PATH:/usr/local/go/bin"
-# go end
+starship init fish | source
