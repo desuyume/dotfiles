57cc7297e0903bdae2e71a9a79c949a9f505e750
desuyume <77desss@gmail.com>
2024-06-26 23:37:03 +0200

    update fish config, add vscode profile

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,3 +5,10 @@ end
 starship init fish | source
 
 set -g fish_greeting
+
+# pnpm
+set -gx PNPM_HOME "/home/desuyume/.local/share/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
